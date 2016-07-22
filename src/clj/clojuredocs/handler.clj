ef60de8355414cba63b8c2395639842b2782392c
Plínio Balduino <pbalduino+github@gmail.com>
2016-07-22 19:33:15 -0300

    handler

--- src/clj/clojuredocs/handler.clj
+++ src/clj/clojuredocs/handler.clj
@@ -15,14 +15,19 @@
 (defn head []
   [:head
    [:meta {:charset "utf-8"}]
+   [:meta {:http-equiv "X-UA-Compatible" :content= "IE=edge"}]
    [:meta {:name    "viewport"
            :content "width=device-width, initial-scale=1"}]
+   [:meta {:name    "description"
+           :content "ClojureDocs BR é o primeiro site colaborativo em Língua Portuguesa contendo a documentação traduzida da linguagem Clojure e exemplos para as suas funções e macros."}]
+
    (include-css (if (env :dev) "/css/site.css" "/css/site.min.css"))
    (include-css (if (env :dev) "/css/bootstrap.css" "/css/bootstrap.min.css"))
-   (include-css (if (env :dev) "/css/bootstrap-theme.css" "/css/bootstrap-theme.min.css"))])
+   (include-css (if (env :dev) "/css/bootstrap-theme.css" "/css/bootstrap-theme.min.css"))
+   [:title "ClojureDocs BR"]])
 
 (def loading-page
-  (html5
+  (html5 {:lang "pt-BR"}
     (head)
     [:body {:class "body-container"}
      mount-target
@@ -31,8 +36,6 @@
 
 (defroutes routes
            (GET "/" [] loading-page)
-           (GET "/about" [] loading-page)
-
            (resources "/")
            (not-found "Not Found"))
 
